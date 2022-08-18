914f43c58e920c180924f9bcc400864de3b34bd6
abhijitprasad4 <94544539+abhijitprasad4@users.noreply.github.com>
2022-08-18 21:10:45 +0530

    Adding new ranking rules
    ranking rules that this program follows is
    more query word matches in the article and after that it follows second rule the more queryword matched in the title
     and third rule which is the sum of all frequency query words

--- abc/main.py
+++ abc/main.py
@@ -15,10 +15,16 @@ for x in list:
 k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16,k17,k18,k19,k20,k21,k22,k23,k24,k25,k26,k27,k28,k29,k30,k31,k32,k33,k34,k35,k36,k37,k38,k39,k40,k41,k42,k43,k44,k45,k46,k47,k48,k49,k50 = {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
 list = [k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16,k17,k18,k19,k20,k21,k22,k23,k24,k25,k26,k27,k28,k29,k30,k31,k32,k33,k34,k35,k36,k37,k38,k39,k40,k41,k42,k43,k44,k45,k46,k47,k48,k49,k50]
 AllWordDict = {}
+# title dictionary for individual articles
+T_k1,T_k2,T_k3,T_k4,T_k5,T_k6,T_k7,T_k8,T_k9,T_k10,T_k11,T_k12,T_k13,T_k14,T_k15,T_k16,T_k17,T_k18,T_k19,T_k20,T_k21,T_k22,T_k23,T_k24,T_k25,T_k26,T_k27,T_k28,T_k29,T_k30,T_k31,T_k32,T_k33,T_k34,T_k35,T_k36,T_k37,T_k38,T_k39,T_k40,T_k41,T_k42,T_k43,T_k44,T_k45,T_k46,T_k47,T_k48,T_k49,T_k50 = {},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
+#list of all title dictionarys
+T_List = [T_k1,T_k2,T_k3,T_k4,T_k5,T_k6,T_k7,T_k8,T_k9,T_k10,T_k11,T_k12,T_k13,T_k14,T_k15,T_k16,T_k17,T_k18,T_k19,T_k20,T_k21,T_k22,T_k23,T_k24,T_k25,T_k26,T_k27,T_k28,T_k29,T_k30,T_k31,T_k32,T_k33,T_k34,T_k35,T_k36,T_k37,T_k38,T_k39,T_k40,T_k41,T_k42,T_k43,T_k44,T_k45,T_k46,T_k47,T_k48,T_k49,T_k50]
+
 # traversing all words of all articles
-for x,kdict in zip((range(1,51)),list):
+for x,kdict,title_dict in zip((range(1,51)),list,T_List):
     x = str(x)
     f = open("news_k50/k"+x+".txt",encoding="utf8")
+    i = 0
     read = f.readlines()
     for line in read:
         for word in line.split():
@@ -30,8 +36,13 @@ for x,kdict in zip((range(1,51)),list):
             if word and not word[-1].isalnum():
                 word = word[:-1]
             word = word.lower()
-            # storing all word of individual article in their individual dictionary
+            # storing all word of individual article in their individual dictionary and titles in their title dict
             if word not in ignoreWordDict:
+                if i == 0:
+                    if word not in title_dict:
+                        title_dict[word] = 1
+                    else:
+                        title_dict[word] += 1
                 if word in kdict:
                     kdict[word] += 1
                 else:
@@ -42,7 +53,13 @@ for x,kdict in zip((range(1,51)),list):
                         AllWordDict[word] = AllWordDict[word] + ["k"+x]
                 else:
                     AllWordDict[word] = ["k"+x]
+        i += 1
     f.close()
+# for x in T_List:
+#     print(x)
+#     print()
+
+
 # Retrival starts here
 # taking inputs
 while 1:
@@ -55,7 +72,6 @@ while 1:
     dictOfAllWord = {"k1_2": [],"k2_2": [],"k3_2": [],"k4_2": [],"k5_2": [],"k6_2": [],"k7_2": [],"k8_2": [],"k9_2": [],"k10_2": [],"k11_2": [],"k12_2": [],"k13_2": [],"k14_2": [],"k15_2": [],"k16_2": [],"k17_2": [],"k18_2": [],"k19_2": [],"k20_2": [],"k21_2": [],"k22_2": [],"k23_2": [],"k24_2": [],"k25_2": [],"k26_2": [],"k27_2": [],"k28_2": [],"k29_2": [],"k30_2": [],"k31_2": [],"k32_2": [],"k33_2": [],"k34_2": [],"k35_2": [],"k36_2": [],"k37_2": [],"k38_2": [],"k39_2": [],"k40_2": [],"k41_2": [],"k42_2": [],"k43_2": [],"k44_2": [],"k45_2": [],"k46_2": [],"k47_2": [],"k48_2": [],"k49_2": [],"k50_2": []}
     n = len(word)
     dictOfAllWord2 = {}
-    list3 = []
     for individual_word in word:
         listOfword = {}
         #check if the word exists in any of the articles or not
@@ -78,27 +94,34 @@ while 1:
             dictOfAllWord2[x] = y
     # print(dictOfAllWord2)
 
-    #now ranking the article first by more number of inputed words matched and second by frequecy of that words
-    while n and len(list3) < 5:
-        dict4 = {}
-        for x,y in dictOfAllWord2.items():
-            if len(y) == n:
-                ab = 0
-                for i in y:
-                    ab += list[int(x[1:-2]) - 1][i]
-                dict4[x] = ab
-        list4 = []
-        list4 = sorted(dict4.keys(), key=lambda x:dict4[x],reverse=True)
-        #storing it in the list3 array
-        list3 += list4
-        n -= 1
+    #creating a new ranking method
+    # 1st by query word all over article
+    # 2nd by query word in the title
+    # 3rd by freq of query word 
+    list3 = []
+    dict4 = {}
+    for article,y in dictOfAllWord2.items():
+        queryWord, titleWord, freq = 0,0,0
+        queryWord = len(y)
+        for i in y:
+            if i in T_List[int(article[1:-2]) -1]:
+                titleWord += T_List[int(article[1:-2]) -1][i]
+            freq += list[int(article[1:-2]) - 1][i]
+        dict4[article] = [queryWord,titleWord,freq]
+    # print()
+    # print(dict4)
 
+    list3 = sorted(dict4.keys(),key=lambda x: dict4[x], reverse=True)
+
+    #removing the k part and printing only the no. of articles
     FinalList = []
-    #removing the k part and printing only the no. of article
     for x in list3[:5]:
         FinalList.append(x[1:-2])
+    
     print("Articles related to your query are:- ")
     print(FinalList)
     print()
+
     # ranking rules that this program follows is
-    # more query word matches in the article and after that it follows second rule which is the sum of all frequecy query words
+    # more query word matches in the article and after that it follows second rule the more queryword matched in the title
+    # and third rule which is the sum of all frequecy query words
